9ab6389116ec67a30ef35fea03d4b188d3158ccf
shaharido1 <shaharido1@gmail.com>
2018-03-19 14:32:25 +0200

    ng-ansyn works as partial component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {appConfig} from 'ng-ansyn/defaultAppConfig'
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+import {AnsynBuilder} from 'ng-ansyn/src/appBuilder/ansyn-builder'
+import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+
+const ansynBuilder = new AnsynBuilder();
+const providers = ansynBuilder.getProvidersFromObject(appConfig);
+const ansynDynamicModule = ansynBuilder.buildModule(providers, true, true);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule,
+    ansynDynamicModule,
   ],
-  providers: [],
+  providers: [RouterModule],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+
+  }
+}
